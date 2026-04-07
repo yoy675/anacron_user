774539eb5b152959e3f750066b246a700af96f98
yoy675 <darobins@g.jct.ac.il>
2026-04-07 04:59:05 +0300

    Initial commit

---
+++ schedule.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MAILTO="$USER"
+LOGFILE="$HOME/.anacron/anacron.log"  # Specify the path for the log file
+last_mod_time=$(stat --printf=%Y "$0")
+for (( current_mod_time=$(stat --printf=%Y "$0"); $current_mod_time == $last_mod_time; current_mod_time=$(stat --printf=%Y "$0") )); do
+	{
+        echo "$(date '+%H:%M:%S') - Starting anacron..."
+        anacron -d -S $HOME/.anacron/spool/anacron -t $HOME/.anacron/anacrontab
+        echo "$(date '+%H:%M:%S') - Finished anacron."
+        echo "Job 'cron.hourly' started."
+        cd $HOME/ && run-parts --report $HOME/.anacron/cron.hourly
+        echo "Job cron.daily terminated on $(date '+%H:%M:%S')."
+    } >> "$LOGFILE" 2>&1  # Append output and errors to the log file
+	sleep 30m
+done
+pkill -P $(jobs | awk '{print $2}')
+$0&
+disown
+exit 0
