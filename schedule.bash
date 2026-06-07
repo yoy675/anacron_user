4c4ca035eddd6d104a35b7952540fe352f4738c2
yoy675 <darobins@g.jct.ac.il>
2026-06-07 19:47:06 +0300

    modified:   anacron-user 	modified:   cron.weekly/anacron_user_log_cleanup 	modified:   schedule.bash

--- schedule.bash
+++ schedule.bash
@@ -11,13 +11,13 @@ while true;do
 	{
 		current_time=$(date '+%-M %-H %-d %-m %-w')
 		cmd=$(~/.anacron/rangeToRegex.py "$(sed '/^ \?#/d;/^ \?[A-Za-z_][A-Za-z0-9_]*=/d;/^$/d;s|#.*||g' $crontab)" "$current_time")
-		eval "$(grep '^ \?[A-Za-z_][A-Za-z0-9_]*=' $crontab)" "$(echo -e $(echo $cmd| sed -z '/[^\\]%/ s|%|\n|'))"
+		eval "$(grep '^ \?[A-Za-z_][A-Za-z0-9_]*=' $crontab)" "$(echo -e $(echo $cmd| sed -z '/[^\\]%/ s|%|\n|'))"&
 		for file in $HOME/.anacron/cron.d/*;do
 			if [ $(sed '/^ \?#/d;/^ \?[A-Za-z_][A-Za-z0-9_]*=/d;/^$/d;s|#.*||g' $file)=='' ];then continue;fi
 			lines=$(~/.anacron/rangeToRegex.py "$(sed '/^ \?#/d;/^ \?[A-Za-z_][A-Za-z0-9_]*=/d;/^$/d;s|#.*||g' $file)" "$current_time")
 			cmd="$(echo "$lines"| awk '{if ($0!="") print "sudo -u "$0}')"
 			if [[ ! -n $cmd ]];then continue; fi
-			eval "$(grep '^ \?[A-Za-z_][A-Za-z0-9_]*=' $file)" "$(echo -e $(echo $cmd| sed -z '/[^\\]%/ s|%|\n|'))"
+			eval "$(grep '^ \?[A-Za-z_][A-Za-z0-9_]*=' $file)" "$(echo -e $(echo $cmd| sed -z '/[^\\]%/ s|%|\n|'))"&
 		done
     } >> "$LOGFILE" 2>&1&  # Append output and errors to the log file
     read last_hour < "$fifo"
